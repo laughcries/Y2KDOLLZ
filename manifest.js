1e94feaa4184816462ef80f77985a3a4bd964f9a
laughcries <amandacetaylor@gmail.com>
2026-04-01 21:04:57 -0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -5,17 +5,7 @@ window.DOLL_MANIFEST = {
   bottoms: [],
   dresses: [],
   accessories: [
-    "assets/tie.gif",
-    "assets/tie16.gif",
-    "assets/tie11.gif",
-    "assets/tie12.gif",
-    "assets/tie13.gif",
-    "assets/tie14.gif",
-    "assets/tie15.gif",
-    "assets/tie9.gif",
-    "assets/tie10.gif",
-    "assets/tie8.gif",
-    "assets/tie7.gif"
+    "assets/tie.gif"
   ]
 };
 
