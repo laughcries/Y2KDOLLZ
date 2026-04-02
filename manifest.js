7cb4195ca56608161ae27e6c3a145d35fce975ea
laughcries <amandacetaylor@gmail.com>
2026-04-01 20:31:45 -0400

    test assets loading

--- manifest.js
+++ manifest.js
@@ -1,98 +1,18 @@
 window.DOLL_MANIFEST = {
-  bodies: [
-    "imgi_10_newbody3.gif",
-    "imgi_11_newbody4.gif",
-    "imgi_2_unique.gif",
-    "imgi_3_newbody1.gif",
-    "imgi_4_Body3.gif",
-    "imgi_5_ub1.gif",
-    "imgi_6_ub2.gif",
-    "imgi_7_bodieh1.gif",
-    "imgi_8_bodiej2.gif",
-    "imgi_9_newbody2.gif"
-  ],
+  bodies: [],
 
   hair: [
-    "imgi_43_lok3.gif",
-    "imgi_44_lok1.gif",
-    "imgi_133_prettydreshoor.gif"
+    "assets/tie.gif"
   ],
 
   tops: [
-    "imgi_58_us11.gif",
-    "imgi_59_us12.gif",
-    "imgi_60_us13.gif",
-    "imgi_61_us14.gif",
-    "imgi_62_us15.gif",
-    "imgi_63_us16.gif",
-    "imgi_64_us18.gif",
-    "imgi_69_us22.gif",
-    "imgi_70_us23.gif",
-    "imgi_71_us24.gif",
-    "imgi_72_us25.gif",
-    "imgi_73_us2.gif",
-    "imgi_74_us3.gif",
-    "imgi_75_us4.gif",
-    "imgi_76_us5.gif",
-    "imgi_77_us8.gif",
-    "imgi_78_us9.gif",
-    "imgi_79_us10.gif"
+    "assets/tie16.gif",
+    "assets/tie11.gif"
   ],
 
-  bottoms: [
-    "imgi_18_legz4.gif",
-    "imgi_23_Legs1.gif",
-    "imgi_25_Legs2.gif",
-    "imgi_27_Legs5.gif",
-    "imgi_36_benen1.gif",
-    "imgi_38_legz3.gif",
-    "imgi_40_legz8.gif",
-    "imgi_45_rokjtrx.gif",
-    "imgi_46_rok1.gif",
-    "imgi_47_rok3.gif",
-    "imgi_48_rokjem4.gif",
-    "imgi_49_Skirt2.gif",
-    "imgi_50_rokja2.gif",
-    "imgi_51_rokja4.gif",
-    "imgi_52_rokja1.gif",
-    "imgi_53_beenrok.gif",
-    "imgi_54_rokkiejjj.gif",
-    "imgi_55_Legs4.gif",
-    "imgi_56_rokja8.gif",
-    "imgi_57_bskirt1.gif",
-    "imgi_84_rok4.gif",
-    "imgi_109_Skirt1.gif"
-  ],
+  bottoms: [],
 
-  dresses: [
-    "imgi_111_Dress3.gif",
-    "imgi_112_Dress4.gif",
-    "imgi_113_Dress2.gif",
-    "imgi_114_ud9.gif",
-    "imgi_115_Dress1.gif",
-    "imgi_116_prettydressmanwlw.gif",
-    "imgi_117_dressalweereen.gif",
-    "imgi_118_jurrak1.gif",
-    "imgi_119_jurrak2.gif",
-    "imgi_120_jurrak5.gif",
-    "imgi_121_jurrak4.gif",
-    "imgi_122_jurrak3.gif",
-    "imgi_123_jurkk1.gif",
-    "imgi_124_ud5.gif",
-    "imgi_125_julk1.gif",
-    "imgi_126_dresssiej5.gif",
-    "imgi_127_dressiej4.gif",
-    "imgi_129_dressiej.gif",
-    "imgi_130_Bdress1.gif",
-    "imgi_131_dressiej3.gif",
-    "imgi_132_dressiej6.gif",
-    "imgi_134_ud1.gif",
-    "imgi_135_ud2.gif",
-    "imgi_136_ud3.gif",
-    "imgi_137_ud4.gif",
-    "imgi_138_ud6.gif",
-    "imgi_139_ud7.gif"
-  ]
+  dresses: []
 };
 
 window.DEFAULT_SELECTIONS = {
