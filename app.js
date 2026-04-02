1faf2e9b2d73120b4e94318d0f4738e0c6c78fd5
laughcries <amandacetaylor@gmail.com>
2026-04-01 22:51:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const categories = [
 ];
 
 const defaultLayerPositions = {
-  bodies: { x: 50, y: 56, scale: 0.95 },
+  bodies: { x: 50, y: 56, scale: 0.65 },
   hair: { x: 50, y: 38, scale: 0.95 },
   tops: { x: 50, y: 56, scale: 0.95 },
   bottoms: { x: 50, y: 56, scale: 0.95 },
